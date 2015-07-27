22d8feaba616378450112acabf7d44da4c0e60fb
Ricardo Fernandez <fernandezr16@gmail.com>
2015-07-27 06:25:19 -0700

    tweaks

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -15,3 +15,11 @@
     $(window).load(function(){
         $('#modal5').modal('show');
     });
+
+
+
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+        
